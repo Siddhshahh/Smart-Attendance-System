eed23cd3cbb608f6dffaedf39893c0fa799686fa
Siddh <siddhshah61@gmail.com>
2023-04-01 11:52:43 +0530

    Initial commit

---
+++ images.py
@@ -0,0 +1,24 @@
+import cv2
+import os
+from flask import Flask, request, render_template
+from datetime import date
+from datetime import datetime
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import joblib
+
+def train_model():
+    faces = []
+    labels = []
+    userlist = os.listdir('faces')
+    for user in userlist:
+        for imgname in os.listdir(f'static/faces/{user}'):
+            img = cv2.imread(f'static/faces/{user}/{imgname}')
+            resized_face = cv2.resize(img, (100, 100))
+            faces.append(resized_face.ravel())
+            labels.append(user)
+    faces = np.array(faces)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(faces, labels)
+    joblib.dump(knn, 'E:/SEM 6/face-recognition-attendance-system/model2.pkl')
